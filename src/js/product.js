3bbb261f2c99735282100c9671066e7f924a6c41
Andrew Parry <parryandrew47@gmail.com>
2024-11-29 09:34:29 -0700

    W02 Team Activity Sleepoutside.

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,12 @@
-// product.js
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage, getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData('tents');
+const productId = getParam('product');
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 function addProductToCart(product) {
     let cart = getLocalStorage('so-cart');
