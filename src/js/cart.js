4a7d13a02dba1aaeac24a6c379560eaa9756eaa1
Andrew Parry <parryandrew47@gmail.com>
2024-11-29 22:35:55 -0700

    implemented subtotal for cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,18 @@
 // cart.js
 import { getLocalStorage } from './utils.mjs';
 
+function updateTotal() {
+    const totalElement = document.querySelector('#cart-total')
+    const cartItems = getLocalStorage('so-cart');
+    let total = 0;
+    cartItems.forEach((item) => {
+        console.log(item);
+        total += item.price * item.quantity;
+    });
+
+    totalElement.innerHTML = `Subtotal: $${total}`;
+    totalElement.style.display = 'block';
+}
 function renderCartContents() {
     const cartItems = getLocalStorage('so-cart');
     if (!cartItems) {
@@ -16,21 +28,23 @@ function renderCartContents() {
 
 function cartItemTemplate(item) {
     const quantity = item.quantity || 1; // Default to 1 if quantity isn't set
+    console.log(item);
     const newItem = `<li class="cart-card divider">
         <a href="#" class="cart-card__image">
             <img
-                src="${item.Image}"
-                alt="${item.Name}"
+                src="${item.image}"
+                alt="${item.name}"
             />
         </a>
         <a href="#">
-            <h2 class="card__name">${item.Name}</h2>
+            <h2 class="card__name">${item.name}</h2>
         </a>
-        <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+        <p class="cart-card__color">${item.color}</p>
         <p class="cart-card__quantity">qty: ${quantity}</p>
-        <p class="cart-card__price">$${(item.FinalPrice * quantity).toFixed(2)}</p>
+        <p class="cart-card__price">$${item.price}</p>
     </li>`;
     return newItem;
 }
 
 renderCartContents();
+updateTotal();
