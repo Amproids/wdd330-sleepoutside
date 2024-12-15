16d17becdb09038e130ac220fe2cf81a5a357905
Andrew Parry <parryandrew47@gmail.com>
2024-12-14 19:23:31 -0700

    added animation for cart and counter for cart

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -45,21 +45,21 @@ export function qs(selector, parent = document) {
     return template;
 }
   
-  export async function loadHeaderFooter() {
-    try {
-        const headerTemplate = await loadTemplate("../partials/header.html");
-        const headerElement = document.querySelector("#main-header");
-        const footerTemplate = await loadTemplate("../partials/footer.html");
-        const footerElement = document.querySelector("#main-footer");
-        if (headerElement && footerElement) {
-            renderWithTemplate(headerTemplate, headerElement);
-            renderWithTemplate(footerTemplate, footerElement);
-        } else {
-            console.error("Header or footer elements not found");
-        }
-    } catch (err) {
-        console.error("Error loading header/footer:", err);
-    }
+export async function loadHeaderFooter() {
+    const header = await loadTemplate("../partials/header.html");
+    const footer = await loadTemplate("../partials/footer.html");
+    document.querySelector("#main-header").innerHTML = header;
+    document.querySelector("#main-footer").innerHTML = footer;
+    
+    // Initialize cart count
+    const cart = JSON.parse(localStorage.getItem('so-cart')) || [];
+    const count = Array.isArray(cart) ? cart.length : 1;
+    
+    let countBadge = document.createElement('div');
+    countBadge.className = 'cart-count';
+    countBadge.textContent = count;
+    countBadge.style.display = count > 0 ? 'flex' : 'none';
+    document.querySelector('.cart').appendChild(countBadge);
 }
 export function alertMessage(message, color = 'red', timer = 5000) {
     const alert = document.createElement('div');
